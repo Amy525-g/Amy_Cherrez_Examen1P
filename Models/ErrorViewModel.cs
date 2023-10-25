780bc2684ee6764d743bd002d8a9858aa13be5e2
Amy Cherrez <amy.cherrez@udla.edu.ec>
2023-10-25 07:50:06 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Amy_Cherrez_Examen1P.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
