543bb104b16b2c94a2e319e2ff7eb48703970a0b
Amy Cherrez <amy.cherrez@udla.edu.ec>
2023-10-25 07:53:52 -0500

    Creacion de la Tabla Factura

---
+++ Models/AmyCherrez_TablaFactura.cs
@@ -0,0 +1,12 @@
+﻿namespace Amy_Cherrez_Examen1P.Models
+{
+    public class AmyCherrez_TablaFactura
+    {
+        public int AmId { get; set; }
+        public string? AmProducto { get; set; }  
+        public string? AmDescription_Producto { get; set; }
+        public bool AmPagoEfectivo { get; set; }
+        public decimal AmPrecio { get; set; }
+
+    }
+}
