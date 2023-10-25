1c5ab1d5b87fdb41feef5f4c4e0b635d448344fe
Amy Cherrez <amy.cherrez@udla.edu.ec>
2023-10-25 08:50:23 -0500

    Se agraga validadciones

--- Models/AmyCherrez_TablaFactura.cs
+++ Models/AmyCherrez_TablaFactura.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.ComponentModel.DataAnnotations;
 
 namespace Amy_Cherrez_Examen1P.Models
 {
@@ -6,10 +7,12 @@ namespace Amy_Cherrez_Examen1P.Models
     {
         public int ID { get; set; }
         [Required]
-        public string? AmProducto { get; set; }  
+        public string? AmProducto { get; set; }
+        [Required]
         public string? AmDescription_Producto { get; set; }
         public bool AmPagoEfectivo { get; set; }
+        [Range(0.50, 99.99)]
         public decimal AmPrecio { get; set; }
-
     }
+
 }
